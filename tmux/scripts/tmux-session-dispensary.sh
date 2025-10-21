74fa5c6542cb8ecb01d02fa036c45c0e76946d80
OfirSinn <Ofir.Sinn@gmail.com>
2025-10-21 20:21:40 +0300

    waybar-better styling
    tmux small fixes
    neovim revert to theme work

--- tmux/scripts/tmux-session-dispensary.sh
+++ tmux/scripts/tmux-session-dispensary.sh
@@ -10,7 +10,7 @@ DIRS=(
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find "${DIRS[@]}" -type d -maxdepth 1 \
+    selected=$(find "${DIRS[@]}" -maxdepth 1 -type d \
         | sed "s|^$HOME/||" \
         | sk --margin 10% --color="bw")
 
@@ -21,9 +21,13 @@ fi
 
 selected_name=$(basename "$selected" | tr . _)
 
-if ! tmux has-session -t "$selected_name"; then
+if [[ -z $TMUX ]] && ! tmux has-session 2>/dev/null; then
+    tmux start-server
+fi
+
+if ! tmux has-session -t "$selected_name" 2> /dev/null; then
     tmux new-session -ds "$selected_name" -c "$selected"
     tmux select-window -t "$selected_name:1"
 fi
 
-tmux switch-client -t "$selected_name"
+tmux switch-client -t "$selected_name" 2> /dev/null || tmux attach -t "$selected_name"
